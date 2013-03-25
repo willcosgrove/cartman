50e25da7cad4e74529c8d2dfecdccccbf4b09e57
Wayne <wayne@blissofbeing.com>
2013-03-25 11:54:10 -0500

    add failing test for #each_with_object using new items('Type') function

--- spec/item_collection_spec.rb
+++ spec/item_collection_spec.rb
@@ -2,8 +2,7 @@ require 'spec_helper'
 
 describe Cartman do
   describe Cartman::ItemCollection do
-    before(:all) do
-    end
+    let(:cart) { Cartman::Cart.new(1) }
 
     before(:each) do
       Cartman.config.redis.flushdb
@@ -11,14 +10,21 @@ describe Cartman do
       Bottle = double
       Bottle.stub(:find).and_return(bottle)
     end
-    
+
     describe "#each_with_object" do
       let(:bottle) { double("Bottle") }
+
       it "should be magical" do
-        cart = Cartman::Cart.new(1)
-        cart.add_item(id: 17, type: "Bottle", name: "Bordeux", unit_cost: 92.12, cost: 184.24, quantity: 2) 
+        cart.add_item(id: 17, type: "Bottle", name: "Bordeux", unit_cost: 92.12, cost: 184.24, quantity: 2)
         expect { |b| cart.items.each_with_object(&b)}.to yield_successive_args([cart.items.first, bottle])
       end
+
+      it "should work with items('Type')" do
+        cart.add_item(id: 17, type: "Bottle", name: "Bordeux", unit_cost: 92.12, cost: 184.24, quantity: 2)
+        cart.add_item(id: 27, type: "UserGiftCard", unit_cost: 25, quantity: 1)
+        expect { |b| cart.items("Bottle").each_with_object(&b)}.to_not raise_error(ArgumentError)
+      end
+
     end
   end
 end
